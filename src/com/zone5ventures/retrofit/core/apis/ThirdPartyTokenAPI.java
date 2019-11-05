361bf5c1ae6481bec9e56b32bc951d6aaf533795
Andrew Hall <andrew@todaysplan.com.au>
2019-11-05 21:52:42 +1100

    Z5-6

---
+++ src/com/zone5ventures/retrofit/core/apis/ThirdPartyTokenAPI.java
@@ -0,0 +1,30 @@
+package com.zone5ventures.retrofit.core.apis;
+
+import com.zone5ventures.core.enums.UserConnectionsType;
+import com.zone5ventures.core.thirdpartyconnections.ThirdPartyToken;
+import com.zone5ventures.core.thirdpartyconnections.ThirdPartyTokenResponse;
+import com.zone5ventures.core.users.Users;
+
+import io.reactivex.Observable;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+
+public interface ThirdPartyTokenAPI {
+	
+	/** Set a 3rd party OAuth token - returns a success: true|false */
+    @POST(Users.SET_THIRD_PARTY_CONNECTION)
+    Observable<Response<ThirdPartyTokenResponse>> setThirdPartyToken(@Body ThirdPartyToken token, @Query("service_name") UserConnectionsType type);
+
+    /** Delete a 3rd party OAuth token - returns a success: true|false */
+    @POST(Users.REM_THIRD_PARTY_CONNECTION)
+    Observable<Response<ThirdPartyTokenResponse>> removeThirdPartyToken(@Query("service_name") UserConnectionsType type);
+    
+    /** Returns details of the current 3rd party token (if any) - returns a available: true|false, token: {...} */
+    @GET(Users.HAS_THIRD_PARTY_CONNECTION)
+    Observable<Response<ThirdPartyTokenResponse>> hasThirdPartyToken(@Query("service_name") UserConnectionsType type);
+
+}
