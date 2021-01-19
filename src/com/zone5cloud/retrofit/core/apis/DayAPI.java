d817a3cb829bfb2d8151c163608eacf088612a8c
vidya <vidya@zone5cloud.com>
2021-01-19 12:23:53 +1100

    SJ-15, Fixing DayAPI and the tests

--- src/com/zone5cloud/retrofit/core/apis/DayAPI.java
+++ src/com/zone5cloud/retrofit/core/apis/DayAPI.java
@@ -7,15 +7,29 @@ import com.zone5cloud.core.search.MappedSearchResult;
 import com.zone5cloud.core.search.SearchInput;
 import io.reactivex.Observable;
 import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 
 public interface DayAPI {
 
+	/**
+	 * Search User Day by setting the search parameters
+	 * @param search json input that sets the search fields and criteria (sports type , date range)
+	 * @param offset
+	 * @param count
+	 * @return
+	 */
 	@POST(Day.SEARCH)
-	Observable<Response<MappedSearchResult<UserDay>>> search(SearchInput<UserDaySearch> search, @Path("offset") int offset, @Path("count") int count);
+	Observable<Response<MappedSearchResult<UserDay>>> search(@Path("offset") int offset, @Path("count") int count, @Body SearchInput<UserDaySearch> search);
 
+	/**
+	 * Allows pagination of the search results to iterate over the search results returned
+	 * @param offset
+	 * @param count
+	 * @return
+	 */
 	@POST(Day.NEXT)
 	Observable<Response<MappedSearchResult<UserDay>>> next(@Path("offset") int offset, @Path("count") int count);
 
